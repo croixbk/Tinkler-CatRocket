545ef689d0ee82ab3ae8bb66d05174ea9669709d
Ivan Ribeiro <ivansnpmaster@gmail.com>
2015-08-07 23:06:46 -0300

    Camera movement

--- Projeto Integrador/Assets/Scripts/CameraMovement.cs
+++ Projeto Integrador/Assets/Scripts/CameraMovement.cs
@@ -1,2 +1,14 @@
-﻿
-A CAMERA EH FIXA CARAI
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+	public float velocity = 12F;
+	Vector3 ahead; 
+ 
+	void Update () 
+	{
+		ahead = Vector3.up * velocity * Time.deltaTime;
+		transform.Translate (ahead); 
+	}
+}
