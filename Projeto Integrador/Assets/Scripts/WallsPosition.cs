6f0cc1d02537a1953e9dec7b50beaebd75ebcc34
Ivan Ribeiro <ivansnpmaster@gmail.com>
2015-08-08 18:35:11 -0300

    Wall script

--- Projeto Integrador/Assets/Scripts/WallsPosition.cs
+++ Projeto Integrador/Assets/Scripts/WallsPosition.cs
@@ -15,33 +15,33 @@ public class WallsPosition : MonoBehaviour
 		{	
 			case Side.Bottom:
 			{
-				position = Camera.main.ScreenToWorldPoint(new Vector3(0, 0.5F, 13));
-				position.y = 2;
-				position.x = 0;
+				position = Camera.main.ScreenToWorldPoint(new Vector3(0F, 0.5F, 13F));
+				position.y = 2F;
+				position.x = 0F;
 				transform.position = position;
 				break;
 			}
 			case Side.Up:
 			{
-				position = Camera.main.ScreenToWorldPoint(new Vector3(0F, 0F, 0 - 13));
-				position.y = 2;
-				position.x = 0;
+				position = Camera.main.ScreenToWorldPoint(new Vector3(0F, 0F, 0 - 13F));
+				position.y = 2F;
+				position.x = 0F;
 				transform.position = position;
 				break;
 			}
 
 			case Side.Right:
 			{
-				position = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -13));
-				position.y = 2;
+				position = Camera.main.ScreenToWorldPoint(new Vector3(0F, 0F, -13));
+				position.y = 2F;
 				position.z = transform.position.z;
 				transform.position = position;
 				break;
 			}
 			case Side.Left:
 			{
-				position = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 13));
-				position.y = 2;
+				position = Camera.main.ScreenToWorldPoint(new Vector3(0F, 0F, 13F));
+				position.y = 2F;
 				position.z = transform.position.z;
 				transform.position = position;
 				break;
