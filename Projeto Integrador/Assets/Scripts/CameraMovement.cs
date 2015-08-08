51b7f608c0c783d65944976a642d24cd28410d2a
Yuri Fernando <yuri.m.g74@gmail.com>
2015-08-07 22:51:47 -0300

    teste 2

--- Projeto Integrador/Assets/Scripts/CameraMovement.cs
+++ Projeto Integrador/Assets/Scripts/CameraMovement.cs
@@ -1,14 +1,2 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CameraMovement : MonoBehaviour 
-{
-	public float velocity = 12F;
-	Vector3 ahead;
-
-	void Update () 
-	{
-		ahead = Vector3.up * velocity * Time.deltaTime;
-		transform.Translate (ahead);
-	}
-}
+﻿
+A CAMERA EH FIXA CARAI
