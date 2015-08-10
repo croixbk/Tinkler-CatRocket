245a1631a0654cad41caaab7f96d98a84b4fd88a
Yuri Fernando <yuri.m.g74@gmail.com>
2015-08-09 22:21:33 -0300

    Little Tests

---
+++ Projeto Integrador/Assets/Scripts/Testes/RotationSphere.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationSphere : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	public float velocity;
+
+	void Start () {
+	}
+	
+	void Update () {
+		if (Input.GetMouseButton(0)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if(Physics.Raycast(ray, out hit) && !hit.transform.tag.Equals("Player")){
+				transform.rotation = Quaternion.Lerp(
+					transform.rotation, 
+					Quaternion.FromToRotation(transform.up, hit.normal)* transform.rotation,
+					velocity * Time.deltaTime);
+
+			}
+		} 
+	}
+}
