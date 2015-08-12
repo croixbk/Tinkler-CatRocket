6a9d48d8d1bebb4db82fd055e594058e5dd8602c
Yuri Fernando <yuri.m.g74@gmail.com>
2015-08-12 02:38:17 -0300

    Player Movement Done

---
+++ Projeto Integrador/Assets/Scripts/LittleThings/FpsDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FpsDisplay : MonoBehaviour
+{
+	float deltaTime = 0.0f;
+	string text;
+	Text textArea;
+
+	void Start(){
+		textArea = GetComponent<Text> ();
+	}
+
+	void Update()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		textArea.text = text;
+	}
+}
--- Projeto Integrador/Assets/Scripts/Main 2/RotationSphere.cs
+++ Projeto Integrador/Assets/Scripts/Main 2/RotationSphere.cs
@@ -3,30 +3,88 @@ using System.Collections;
 
 public class RotationSphere : MonoBehaviour 
 {
-	public float velocityEnemy;
-	public float factor = 3.1F;
-
-	float velocityWhenBall;
-
+	public float velocity;
+	public float outVelocity;
+	Quaternion rotationPosition;
+	public GameObject edgeCube;
+	public LayerMask layerMask;
+	public LayerMask edgeCubeLM;
 	void Start()
 	{
-		velocityWhenBall = velocityEnemy / factor;
+
 	}
 
 	void Update () 
 	{
-		Rotate ();
+		#if UNITY_EDITOR
+			MouseMovement();	
+		#endif
+			TouchMovement ();
 	}
 
-	void Rotate()
+	void MovementEdgeCube(Ray ray){
+			RaycastHit hit;
+			if(Physics.Raycast(ray,out hit,edgeCubeLM)){
+				edgeCube.transform.position = hit.point;
+			}
+
+	}
+
+	void MouseMovement()
 	{
-		if (Input.GetMouseButton (0)) 
-		{
+		if (Input.GetMouseButton (0)) {
+
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
-		
-			if (Physics.Raycast (ray, out hit))
-				transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.FromToRotation (transform.up, (hit.transform.tag.Equals ("Ball")) ? -hit.normal : hit.normal) * transform.rotation, (hit.transform.tag.Equals ("Ball") ? velocityWhenBall : velocityEnemy) * Time.deltaTime);
+
+			if (Physics.Raycast (ray, out hit)) {
+				if(hit.transform.name.Equals("Plane")){
+					MovementEdgeCube(ray);
+					//Debug.DrawLine(edgeCube.transform.position,transform.position,Color.red,Mathf.Infinity,false);
+					if(Physics.Linecast(edgeCube.transform.position,transform.position,out hit,layerMask)){
+						rotationPosition =  Quaternion.Lerp (transform.rotation,
+					                                     Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation,
+					                                     outVelocity * Time.deltaTime);
+					}
+				}
+				else{
+					rotationPosition =  Quaternion.Lerp (transform.rotation,
+														Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation,
+					                                    velocity * Time.deltaTime);
+				}
+			}
 		}
+		transform.rotation = rotationPosition;
+
+
+	}
+//--------O MouseMovement Funciona normalmente no celular(nao precisa do touch), mas vai que neh-----------------------//
+	void TouchMovement(){
+		if(Input.touches.Length != 0){
+			Ray ray;
+			RaycastHit hit;
+			foreach(Touch touch in Input.touches){
+				ray = Camera.main.ScreenPointToRay (touch.position);
+
+				if (Physics.Raycast (ray, out hit)) {
+					print(hit.transform.name);
+					if(hit.transform.name.Equals("Plane")){
+						MovementEdgeCube(ray);
+						if(Physics.Linecast(edgeCube.transform.position,transform.position,out hit,layerMask)){
+							rotationPosition =  Quaternion.Lerp (transform.rotation,
+							                                     Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation,
+							                                     outVelocity * Time.deltaTime);
+						}
+					}
+					else{
+						rotationPosition =  Quaternion.Lerp (transform.rotation,
+						                                     Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation,
+						                                     velocity * Time.deltaTime);
+					}
+				}
+			}
+			transform.rotation = rotationPosition;
+		}
+
 	}
 }
